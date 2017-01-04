a3856c1924c84c03a44fd8e141b497d528268538
charlespaim <cpaimdas@hotmail.com>
2017-01-04 17:11:04 -0300

    Otimização na rotina

--- #Likes_Hash_Location.js
+++ #Likes_Hash_Location.js
@@ -1,4 +1,10 @@
-﻿function possuiValorArray(objArray, objValor){
+﻿function sorteia(numeroMaximo){
+	//sorteia um númeiro inteiro aleatório entre 1 o número máximo definido (incluindo os extremos)
+	return Math.floor(Math.random() * numeroMaximo ) + 1;
+}
+
+function possuiValorArray(objArray, objValor){
+	//adiciona um valor ao array caso ele não exista
 	if (objArray.indexOf(objValor)> -1) {
 		return true;
 	} else {
@@ -7,34 +13,29 @@
 	return false;
 }
 
+var header = "CODE:\nSET !TIMEOUT_TAG 0\n";
+var labelLike = ["Like","Curtir","Gosto"];
+var labelNext = ["Next","Próximo","Seguinte"];
 function fotoPossuiLike(){
-
-	var macro =  "CODE:";
-	macro +=  "SET !TIMEOUT_TAG 0" + "\n";
-	macro +=  'TAG POS=1 TYPE=SPAN ATTR=TXT:Like EXTRACT=TXT' + "\n"; 
 	
-	var ret = iimPlay(macro);
-	if (ret< 1) {
-		 macro =  "CODE:";
-		macro +=  "SET !TIMEOUT_TAG 0" + "\n";
-		macro +=  'TAG POS=1 TYPE=SPAN ATTR=TXT:Curtir EXTRACT=TXT' + "\n"; 
-		var ret = iimPlay(macro);
-		
-		if (ret< 1) {
-			 macro =  "CODE:";
-			macro +=  "SET !TIMEOUT_TAG 0" + "\n";
-			macro +=  'TAG POS=1 TYPE=SPAN ATTR=TXT:Gosto EXTRACT=TXT' + "\n"; 
-			var ret = iimPlay(macro);
-			if (ret< 1) {
-				return false;
-			}
-		}	
+	//returna true se a foto ainda não foi curtida
+	var cont = 0;
+	var retlike = -1;
+	while (cont <= labelLike.length & retlike == -1 ) {
+		var like =  header + "TAG POS=1 TYPE=SPAN ATTR=TXT:" + labelLike[cont] + " EXTRACT=TXT" + "\n";
+		retlike = iimPlay(like);
+		if (retlike == 1) {		
+			return true;
+		}
+		cont++;
 	}
-	return true;	
+	
+	return false;
 }
 
 function getperfil() {	
 
+		//retorna o nome do perfil da foto atualmente aberta
 		var macro =  "CODE:";
 		macro +=  'TAG POS=1 TYPE=A ATTR=class:"*notranslate*" EXTRACT=HREF' + "\n"; 
 		var ret = iimPlay(macro);
@@ -45,13 +46,15 @@ function getperfil() {
 
 function abreprimeirafoto(url) {
 	//abre a primeira foto		
+	var pos = [];
 	var macro =  "CODE:";
 	macro +=  "URL GOTO=" + url + "\n"; 
 	if (url.indexOf("/tags/") > -1 ) {
-		macro +=  "CLICK X=316 Y=453" + "\n"; 	
+		pos = ["316","453"];
 	} else {
-		macro +=  "CLICK X=601 Y=601" + "\n"; 	
+		pos = ["601","601"];
 	}
+	macro +=  "CLICK X=" + pos[0] + " Y=" + pos[1] + "\n"; 	
 	
 	iimPlay(macro);	
 	
@@ -63,8 +66,7 @@ function abreprimeirafoto(url) {
 		macro +=  "TAG POS=1 TYPE=A ATTR=TXT:Next" + "\n"; 
 		macro +=  "TAG POS=1 TYPE=A ATTR=TXT:Próximo" + "\n"; 
 		macro +=  "TAG POS=1 TYPE=A ATTR=TXT:Seguinte" + "\n"; 
-		segundos = Math.floor(Math.random() * 3 ) + 1;	
-		macro +=  "WAIT SECONDS=" + segundos + "\n"; 
+		macro +=  "WAIT SECONDS=" + sorteia(3) + "\n"; 
 		iimPlay(macro);
 	}	
 }
@@ -128,71 +130,53 @@ var abas= [
 
 //Número de likes por aba
 var numlikes=prompt("Entre com a quantidade de likes", "50");
-var segundos;
 var maximoSegundos = 15;
 var likes = 0;
 
 for (var y=0;y<abas.length;y++) {	
-	var curtidos = 0;
 
 	abreprimeirafoto(abas[y]);	
 	
-	var header = "CODE:\nSET !TIMEOUT_TAG 0\n";
-	
-	var like =  header + "TAG POS=1 TYPE=SPAN ATTR=TXT:Like" + "\n"; 
-	var curtir = header + "TAG POS=1 TYPE=SPAN ATTR=TXT:Curtir" + "\n";
-	var gosto = header + "TAG POS=1 TYPE=SPAN ATTR=TXT:Gosto" + "\n";	
+	var curtidos = 0;
+	var likesAba = 0;
+	while (likesAba < numlikes & curtidos < 5 ) {
 		
-	var next = header + "TAG POS=1 TYPE=A ATTR=TXT:Next" + "\n"; 
-	var proximo = header + "TAG POS=1 TYPE=A ATTR=TXT:Próximo" + "\n"; 
-	var seguinte = header + "TAG POS=1 TYPE=A ATTR=TXT:Seguinte" + "\n"; 
-	
-	for (var x=0;x<numlikes;x++) {		
-	
+		//Se nenhuma foto do perfil atual foi curtida, curte a foto atual
 		if (fotoPossuiLike() && !possuiValorArray(perfis, getperfil())) {
-		
-			//Curte a foto atual
-			var ret1 = iimPlay(like);	
-			if (ret1 < 0) {		
-				ret1 = iimPlay(curtir);
-				if (ret1 < 0) {	
-					ret1 = iimPlay(gosto);
-					if (ret1 == 1) {							
-						likes++;
-					}
-				} else {
+			var cont = 0;
+			var retlike = -1;
+			while (cont <= labelLike.length & retlike == -1 ) {
+			   	var like =  header + "TAG POS=1 TYPE=SPAN ATTR=TXT:" + labelLike[cont] + "\n";
+				retlike = iimPlay(like);
+				if (retlike == 1) {		
+					likesAba++;				
 					likes++;
 				}
-			} else {
-				likes++;
+				cont++;
 			}
 		} else {
-			x--;
 			curtidos++;
-			if (curtidos == 5) {
-				x = numlikes;
-			}			
-		}
+		}	
 		
-		var ret2 = iimPlay(next);
-		if (ret2 < 0) {		
-			ret2 = iimPlay(proximo);
-			if (ret2 < 0) {		
-				ret2 = iimPlay(seguinte);
-				if (ret2 < 0) {		
-					abreprimeirafoto(abas[y]);	
-				}
-			} 
+		//Para a próximo foto, caso o limite de curtidas por url não tenha sido atingido
+		if (likesAba < numlikes) {
+			cont = 0;
+			var retnext = -1;
+			while (cont <= labelNext.length & retnext < 0 ) {
+				var next =  header + "TAG POS=1 TYPE=A ATTR=TXT:" + labelNext[cont] + "\n";
+				retnext = iimPlay(next);		
+				cont++;
 		}
-		segundos = Math.floor(Math.random() * maximoSegundos ) + 1;	
+			
+			//Recarrega a página caso ocorra erro de página não encontrada
+			if (retnext < 0) {		
+				abreprimeirafoto(abas[y]);	
+			}
+		}
+
 		var macroSegundos = "CODE:";
-		macroSegundos +=  "WAIT SECONDS=" + segundos + "\n"; 
-		iimPlay(macroSegundos)
-	}
-	if (y == abas.length-1) {
-		alert("Total de Likes: " + likes);
-		/*var macroclose =  "CODE:";
-		macroclose +=  "TAB CLOSE" + "\n"; 		
-		iimPlay(macroclose)*/
+		macroSegundos +=  "WAIT SECONDS=" + sorteia(maximoSegundos)+ "\n"; 
+		iimPlay(macroSegundos);
 	}
 }
+alert("Total de urls: " + abas.length + "\nTotal de Likes: " + likes);
